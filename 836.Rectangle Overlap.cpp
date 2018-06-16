416e2ace715415a0ef7faf3c4face3ecbda7755a
waji785 <acsorceress@gmail.com>
2018-06-16 17:32:35 +0800

    摩德维尔
    专业评估过了...朋友圈都在普天同庆

---
+++ 836.Rectangle Overlap.cpp
@@ -0,0 +1,12 @@
+//rank:n
+class Solution {
+public:
+    bool isRectangleOverlap(vector<int>& rec1, vector<int>& rec2) {
+        return !(rec1[2] <= rec2[0] ||   // left
+                 rec1[3] <= rec2[1] ||   // bottom
+                 rec1[0] >= rec2[2] ||   // right
+                 rec1[1] >= rec2[3]);    // top
+                 //如果写成相减并画图就比较好理解了
+        
+    }
+};
